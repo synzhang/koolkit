f5d5680c3ecb462e815b45d47defd7277d8b93ac
Syn Zhang <zeyanzhang.china@gmail.com>
2020-02-17 23:19:49 +0800

    doc: doc for waitForever and waitForTime

--- src/waitForTime.ts
+++ src/waitForTime.ts
@@ -1,3 +1,10 @@
+/**
+ * Wait a given amount of time between tasks.
+ *
+ * @returns A promise which will resolves in given period.
+ *
+ * @link https://davidwalsh.name/waitfortime
+ */
 export default function waitForTime(ms: number): Promise<{}> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
--- src/waitForever.ts
+++ src/waitForever.ts
@@ -1,3 +1,12 @@
+/**
+ * Halts following actions without locking the browser,
+ * so you can interact with the page. Useful in debug and test.
+ *
+ * @returns A promise which never resolves.
+ *
+ * @link https://davidwalsh.name/javascript-wait
+ */
+
 export default function waitForever(): Promise<{}> {
   return new Promise(() => {})
 }
