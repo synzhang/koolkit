21b261014df136a531b4870137ef5eaefaeda4cf
Syn Zhang <zeyanzhang.china@gmail.com>
2020-03-03 23:42:04 +0800

    doc: generate doc

---
+++ docs/assets/js/search.js
@@ -0,0 +1,60 @@
+var typedoc = typedoc || {}
+typedoc.search = typedoc.search || {}
+typedoc.search.data = {
+  kinds: { "1": "External module", "64": "Function", "16777216": "Reference" },
+  rows: [
+    {
+      id: 0,
+      kind: 1,
+      name: '"waitForever"',
+      url: "modules/_waitforever_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 1,
+      kind: 64,
+      name: "waitForever",
+      url: "modules/_waitforever_.html#waitforever",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"waitForever"'
+    },
+    {
+      id: 2,
+      kind: 1,
+      name: '"waitForTime"',
+      url: "modules/_waitfortime_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 3,
+      kind: 64,
+      name: "waitForTime",
+      url: "modules/_waitfortime_.html#waitfortime",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"waitForTime"'
+    },
+    {
+      id: 4,
+      kind: 1,
+      name: '"index"',
+      url: "modules/_index_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 5,
+      kind: 16777216,
+      name: "waitForever",
+      url: "modules/_index_.html#waitforever",
+      classes: "tsd-kind-reference tsd-parent-kind-external-module",
+      parent: '"index"'
+    },
+    {
+      id: 6,
+      kind: 16777216,
+      name: "waitForTime",
+      url: "modules/_index_.html#waitfortime",
+      classes: "tsd-kind-reference tsd-parent-kind-external-module",
+      parent: '"index"'
+    }
+  ]
+}
