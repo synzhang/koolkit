a90a97dc5fe652170de088e61ba9cec6216fc076
Syn Zhang <zeyanzhang.china@gmail.com>
2020-03-22 21:33:44 +0800

    chore: add build directory to git

---
+++ build/waitForTime.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Wait a given amount of time between tasks.
+ *
+ * @returns A promise which will resolves in given period.
+ *
+ * @link https://davidwalsh.name/waitfortime
+ */
+export default function waitForTime(ms: number): Promise<void>;
---
+++ build/waitForever.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Halts following actions without locking the browser,
+ * so you can interact with the page. Useful in debug and test.
+ *
+ * @returns A promise which never resolves.
+ *
+ * @link https://davidwalsh.name/javascript-wait
+ */
+export default function waitForever(): Promise<void>;
