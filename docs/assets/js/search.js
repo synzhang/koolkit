2316fb72a391f84dfa625c7def2ca79cfafe39f7
Syn Zhang <zeyanzhang.china@gmail.com>
2021-07-07 13:05:33 +0800

    doc: generate doc

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,52 +1,44 @@
-var typedoc = typedoc || {}
-typedoc.search = typedoc.search || {}
-typedoc.search.data = {
-  kinds: { "1": "External module", "64": "Function" },
+window.searchData = {
+  kinds: { "64": "Function" },
   rows: [
     {
       id: 0,
-      kind: 1,
-      name: '"partition"',
-      url: "modules/_partition_.html",
-      classes: "tsd-kind-external-module"
+      kind: 64,
+      name: "waitForever",
+      url: "modules.html#waitforever",
+      classes: "tsd-kind-function"
     },
     {
       id: 1,
       kind: 64,
-      name: "partition",
-      url: "modules/_partition_.html#partition",
-      classes: "tsd-kind-function tsd-parent-kind-external-module",
-      parent: '"partition"'
+      name: "waitForTime",
+      url: "modules.html#waitfortime",
+      classes: "tsd-kind-function"
     },
     {
       id: 2,
-      kind: 1,
-      name: '"waitForTime"',
-      url: "modules/_waitfortime_.html",
-      classes: "tsd-kind-external-module"
-    },
-    {
-      id: 3,
       kind: 64,
-      name: "waitForTime",
-      url: "modules/_waitfortime_.html#waitfortime",
-      classes: "tsd-kind-function tsd-parent-kind-external-module",
-      parent: '"waitForTime"'
-    },
-    {
-      id: 4,
-      kind: 1,
-      name: '"waitForever"',
-      url: "modules/_waitforever_.html",
-      classes: "tsd-kind-external-module"
-    },
-    {
-      id: 5,
-      kind: 64,
-      name: "waitForever",
-      url: "modules/_waitforever_.html#waitforever",
-      classes: "tsd-kind-function tsd-parent-kind-external-module",
-      parent: '"waitForever"'
+      name: "partition",
+      url: "modules.html#partition",
+      classes: "tsd-kind-function"
     }
-  ]
+  ],
+  index: {
+    version: "2.3.9",
+    fields: ["name", "parent"],
+    fieldVectors: [
+      ["name/0", [0, 9.808]],
+      ["parent/0", []],
+      ["name/1", [1, 9.808]],
+      ["parent/1", []],
+      ["name/2", [2, 9.808]],
+      ["parent/2", []]
+    ],
+    invertedIndex: [
+      ["partition", { _index: 2, name: { "2": {} }, parent: {} }],
+      ["waitforever", { _index: 0, name: { "0": {} }, parent: {} }],
+      ["waitfortime", { _index: 1, name: { "1": {} }, parent: {} }]
+    ],
+    pipeline: []
+  }
 }
