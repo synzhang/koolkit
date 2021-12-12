fb7fce6d261cb146a6bff96e0735de0560b7915a
Syn Zhang <zeyanzhang.china@gmail.com>
2021-12-12 23:06:05 +0800

    doc: generate doc

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -21,21 +21,31 @@ window.searchData = {
       name: "partition",
       url: "modules.html#partition",
       classes: "tsd-kind-function"
+    },
+    {
+      id: 3,
+      kind: 64,
+      name: "pipe",
+      url: "modules.html#pipe",
+      classes: "tsd-kind-function"
     }
   ],
   index: {
     version: "2.3.9",
     fields: ["name", "parent"],
     fieldVectors: [
-      ["name/0", [0, 9.808]],
+      ["name/0", [0, 12.04]],
       ["parent/0", []],
-      ["name/1", [1, 9.808]],
+      ["name/1", [1, 12.04]],
       ["parent/1", []],
-      ["name/2", [2, 9.808]],
-      ["parent/2", []]
+      ["name/2", [2, 12.04]],
+      ["parent/2", []],
+      ["name/3", [3, 12.04]],
+      ["parent/3", []]
     ],
     invertedIndex: [
       ["partition", { _index: 2, name: { "2": {} }, parent: {} }],
+      ["pipe", { _index: 3, name: { "3": {} }, parent: {} }],
       ["waitforever", { _index: 0, name: { "0": {} }, parent: {} }],
       ["waitfortime", { _index: 1, name: { "1": {} }, parent: {} }]
     ],
