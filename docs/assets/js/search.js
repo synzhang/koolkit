a8f30808e0ff0a7101b94cc5b02115cec0cd6384
Syn Zhang <zeyanzhang.china@gmail.com>
2021-12-19 23:07:38 +0800

    doc: update doc

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -25,6 +25,13 @@ window.searchData = {
     {
       id: 3,
       kind: 64,
+      name: "poll",
+      url: "modules.html#poll",
+      classes: "tsd-kind-function"
+    },
+    {
+      id: 4,
+      kind: 64,
       name: "pipe",
       url: "modules.html#pipe",
       classes: "tsd-kind-function"
@@ -34,18 +41,21 @@ window.searchData = {
     version: "2.3.9",
     fields: ["name", "parent"],
     fieldVectors: [
-      ["name/0", [0, 12.04]],
+      ["name/0", [0, 13.863]],
       ["parent/0", []],
-      ["name/1", [1, 12.04]],
+      ["name/1", [1, 13.863]],
       ["parent/1", []],
-      ["name/2", [2, 12.04]],
+      ["name/2", [2, 13.863]],
       ["parent/2", []],
-      ["name/3", [3, 12.04]],
-      ["parent/3", []]
+      ["name/3", [3, 13.863]],
+      ["parent/3", []],
+      ["name/4", [4, 13.863]],
+      ["parent/4", []]
     ],
     invertedIndex: [
       ["partition", { _index: 2, name: { "2": {} }, parent: {} }],
-      ["pipe", { _index: 3, name: { "3": {} }, parent: {} }],
+      ["pipe", { _index: 4, name: { "4": {} }, parent: {} }],
+      ["poll", { _index: 3, name: { "3": {} }, parent: {} }],
       ["waitforever", { _index: 0, name: { "0": {} }, parent: {} }],
       ["waitfortime", { _index: 1, name: { "1": {} }, parent: {} }]
     ],
