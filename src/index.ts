52176d4a494848ae055f0e70c6852f75bae84a4e
Syn Zhang <zeyanzhang.china@gmail.com>
2021-12-19 23:06:26 +0800

    feat: create poll

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as waitForever } from "./waitForever"
 export { default as waitForTime } from "./waitForTime"
 export { default as partition } from "./partition"
+export { default as poll } from "./poll"
 export { default as pipe } from "./pipe"
---
+++ src/poll.ts
@@ -0,0 +1,41 @@
+/**
+ * The polling function.
+ *
+ * @returns A Promise.
+ *
+ * @link https://1loc.dev/function/compose-functions-from-left-to-right
+ *
+ * @usage
+ *  poll(function() {
+ *    return document.getElementById('lightbox').offsetWidth > 0;
+ *  }, 2000, 150).then(function() {
+ *      // Polling done, now do something else!
+ *  }).catch(function() {
+ *      // Polling timed out, handle the error!
+ *  });
+ */
+
+const poll = (fn, timeout, interval) => {
+  var endTime = Number(new Date()) + (timeout || 2000)
+  interval = interval || 100
+
+  var checkCondition = function(resolve, reject) {
+    // If the condition is met, we're done!
+    var result = fn()
+    if (result) {
+      resolve(result)
+    }
+    // If the condition isn't met but the timeout hasn't elapsed, go again
+    else if (Number(new Date()) < endTime) {
+      setTimeout(checkCondition, interval, resolve, reject)
+    }
+    // Didn't match and too much time, reject!
+    else {
+      reject(new Error("timed out for " + fn + ": " + arguments))
+    }
+  }
+
+  return new Promise(checkCondition)
+}
+
+export default poll
