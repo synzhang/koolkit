eb7ef8dcc291f7bd45247677c1ce874f9a3fefd0
Syn Zhang <zeyanzhang.china@gmail.com>
2022-02-09 10:32:42 +0800

    docs: update doc

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -35,24 +35,44 @@ window.searchData = {
       name: "pipe",
       url: "modules.html#pipe",
       classes: "tsd-kind-function"
+    },
+    {
+      id: 5,
+      kind: 64,
+      name: "descartes",
+      url: "modules.html#descartes",
+      classes: "tsd-kind-function"
+    },
+    {
+      id: 6,
+      kind: 64,
+      name: "generatePrimeNumbers",
+      url: "modules.html#generateprimenumbers",
+      classes: "tsd-kind-function"
     }
   ],
   index: {
     version: "2.3.9",
     fields: ["name", "parent"],
     fieldVectors: [
-      ["name/0", [0, 13.863]],
+      ["name/0", [0, 16.74]],
       ["parent/0", []],
-      ["name/1", [1, 13.863]],
+      ["name/1", [1, 16.74]],
       ["parent/1", []],
-      ["name/2", [2, 13.863]],
+      ["name/2", [2, 16.74]],
       ["parent/2", []],
-      ["name/3", [3, 13.863]],
+      ["name/3", [3, 16.74]],
       ["parent/3", []],
-      ["name/4", [4, 13.863]],
-      ["parent/4", []]
+      ["name/4", [4, 16.74]],
+      ["parent/4", []],
+      ["name/5", [5, 16.74]],
+      ["parent/5", []],
+      ["name/6", [6, 16.74]],
+      ["parent/6", []]
     ],
     invertedIndex: [
+      ["descartes", { _index: 5, name: { "5": {} }, parent: {} }],
+      ["generateprimenumbers", { _index: 6, name: { "6": {} }, parent: {} }],
       ["partition", { _index: 2, name: { "2": {} }, parent: {} }],
       ["pipe", { _index: 4, name: { "4": {} }, parent: {} }],
       ["poll", { _index: 3, name: { "3": {} }, parent: {} }],
