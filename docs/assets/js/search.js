d0e43ee2dd78e0283bef794512a601a3a492526d
Syn Zhang <zeyanzhang.china@gmail.com>
2022-02-12 18:05:01 +0800

    docs: update doc

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -63,32 +63,52 @@ window.searchData = {
       name: "getURLParams",
       url: "modules.html#geturlparams",
       classes: "tsd-kind-function"
+    },
+    {
+      id: 9,
+      kind: 64,
+      name: "arrayMove",
+      url: "modules.html#arraymove",
+      classes: "tsd-kind-function"
+    },
+    {
+      id: 10,
+      kind: 64,
+      name: "dataURIToBlob",
+      url: "modules.html#datauritoblob",
+      classes: "tsd-kind-function"
     }
   ],
   index: {
     version: "2.3.9",
     fields: ["name", "parent"],
     fieldVectors: [
-      ["name/0", [0, 18.971]],
+      ["name/0", [0, 20.794]],
       ["parent/0", []],
-      ["name/1", [1, 18.971]],
+      ["name/1", [1, 20.794]],
       ["parent/1", []],
-      ["name/2", [2, 18.971]],
+      ["name/2", [2, 20.794]],
       ["parent/2", []],
-      ["name/3", [3, 18.971]],
+      ["name/3", [3, 20.794]],
       ["parent/3", []],
-      ["name/4", [4, 18.971]],
+      ["name/4", [4, 20.794]],
       ["parent/4", []],
-      ["name/5", [5, 18.971]],
+      ["name/5", [5, 20.794]],
       ["parent/5", []],
-      ["name/6", [6, 18.971]],
+      ["name/6", [6, 20.794]],
       ["parent/6", []],
-      ["name/7", [7, 18.971]],
+      ["name/7", [7, 20.794]],
       ["parent/7", []],
-      ["name/8", [8, 18.971]],
-      ["parent/8", []]
+      ["name/8", [8, 20.794]],
+      ["parent/8", []],
+      ["name/9", [9, 20.794]],
+      ["parent/9", []],
+      ["name/10", [10, 20.794]],
+      ["parent/10", []]
     ],
     invertedIndex: [
+      ["arraymove", { _index: 9, name: { "9": {} }, parent: {} }],
+      ["datauritoblob", { _index: 10, name: { "10": {} }, parent: {} }],
       ["descartes", { _index: 5, name: { "5": {} }, parent: {} }],
       ["encodeurl", { _index: 7, name: { "7": {} }, parent: {} }],
       ["generateprimenumbers", { _index: 6, name: { "6": {} }, parent: {} }],
