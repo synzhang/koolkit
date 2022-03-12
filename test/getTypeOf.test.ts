481f43d0ad8253d70b693ab7aa96d6604674e5de
Syn Zhang <zeyanzhang.china@gmail.com>
2022-03-12 23:16:54 +0800

    fix: change test environment fot getTypeOf

--- test/getTypeOf.test.ts
+++ test/getTypeOf.test.ts
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import getTypeOf from "../src/getTypeOf"
 
 test("getTypeof", () => {
@@ -13,7 +17,7 @@ test("getTypeof", () => {
   expect(getTypeOf(/[a-z]/g)).toBe("RegExp")
   expect(getTypeOf(new Date(2021))).toBe("Date")
   expect(getTypeOf(new Error())).toBe("Error")
-  expect(getTypeOf(function() {})).toBe("Function")
+  expect(getTypeOf(function () {})).toBe("Function")
   expect(getTypeOf((a, b) => a + b)).toBe("Function")
   expect(getTypeOf(async () => {})).toBe("AsyncFunction")
   expect(getTypeOf(document)).toBe("Document")
