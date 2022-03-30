b0aa2fe87ecfcddd1be97539d9a20f06e108b0ac
Syn Zhang <zeyanzhang.china@gmail.com>
2022-03-30 23:18:31 +0800

    feat: create pipeAsyncFunctions

---
+++ src/pipeAsyncFunctions.ts
@@ -0,0 +1,25 @@
+/**
+ * Performs left-to-right function composition for asynchronous functions.
+ * @param fns Functions to compose.
+ * @returns A function that composes the functions in `fns`.
+ * @link https://www.30secondsofcode.org/js/s/pipe-async-functions
+ * @example
+ * ```ts
+ * const sum = pipeAsyncFunctions(
+ *   x => x + 1,
+ *   x => new Promise(resolve => setTimeout(() => resolve(x + 2), 1000)),
+ *   x => x + 3,
+ *   async x => (await x) + 4
+ * );
+ * (async() => {
+ *   console.log(await sum(5)); // 15 (after one second)
+ * })();
+ * ```
+ */
+
+const pipeAsyncFunctions =
+  (...fns) =>
+  (arg) =>
+    fns.reduce((p, f) => p.then(f), Promise.resolve(arg))
+
+export default pipeAsyncFunctions
